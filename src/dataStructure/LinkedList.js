f91b4fd8195d1e82f44b3dc18f76d9ca6944f37b
Mutrix <yuquanwang1990@icloud.com>
2018-08-03 00:05:07 +0800

    LinkedList需要实现Iterator协议，以便使用for

--- src/dataStructure/LinkedList.js
+++ src/dataStructure/LinkedList.js
@@ -2,7 +2,22 @@
  * Created by mutrix on 2018/8/2.
  */
 export default class LinkedList {
-  constructor() {
-    this.a = '';
+  constructor(items) {
+    this.head = {
+      next: () => {},
+    };
+    this.tail = null;
   }
+
+  contains(ele) {
+    for (const item of this.items) {
+      if (item === ele) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+export class LinkedListNode {
 }
--- test/unit/linkedSet.spec.js
+++ test/unit/linkedSet.spec.js
@@ -1,11 +1,32 @@
 /**
  * Created by mutrix on 2018/8/2.
  */
-import LinkedList from '../../src/dataStructure/LinkedList';
+import LinkedList, {LinkedListNode} from '../../src/dataStructure/LinkedList';
 
 describe('linked List', () => {
   it('存在并可new LinkedList', () => {
     const linkedList = new LinkedList();
     expect(linkedList).toBeInstanceOf(LinkedList);
+  });
+
+  it('具有首尾两个节点', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.head).toBeDefined();
+    expect(linkedList.tail).toBeDefined();
+  });
+
+  it.skip('可以接受数组', () => {
+    const arr = [2, 4, 5];
+    const linkedList = new LinkedList(arr);
+    expect(linkedList.contains(4)).toBeTruthy();
+    expect(linkedList.contains(1)).toBeFalsy();
+  });
+});
+
+describe('LinkedList的节点实现了iterator协议', () => {
+  it('节点具有next方法', () => {
+    const linkdeList = new LinkedList();
+    expect(linkdeList.head).toBeInstanceOf(LinkedListNode);
+    expect(typeof linkdeList.head.next).toBe('function');
   });
 });
