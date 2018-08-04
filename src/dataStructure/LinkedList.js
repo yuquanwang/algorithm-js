9e71c54f75e4a3bb3be439479fcf78aa9af1ac9c
Mutrix <yuquanwang1990@icloud.com>
2018-08-04 23:03:42 +0800

    实现constructor() remove() getLength() has() insertAfter()等方法

--- src/dataStructure/LinkedList.js
+++ src/dataStructure/LinkedList.js
@@ -2,6 +2,8 @@
  * Created by mutrix on 2018/8/2.
  */
 
+import { isEqual } from 'lodash';
+
 export class LinkedListNode {
   constructor(item) {
     if (item === undefined || item === null) {
@@ -13,10 +15,23 @@ export class LinkedListNode {
 }
 
 export default class LinkedList {
-  constructor() {
+  constructor(...args) {
     this.head = null;
     this.tail = null;
-    this[Symbol.iterator] = function* () {
+
+    if (args.length === 1) {
+      if (typeof args[0] === 'object') {
+        this.constructor(...args[0]);
+      } else {
+        this.append(args[0]);
+      }
+    } else {
+      for (const item of args) {
+        this.append(item);
+      }
+    }
+
+    this[Symbol.iterator] = function*() {
       let node = this.head;
       while (node.next) {
         yield node.value;
@@ -24,6 +39,24 @@ export default class LinkedList {
       }
       yield node.value;
     };
+  }
+
+  getLength() {
+    let node = this.head;
+    let length = node ? 1 : 0;
+
+    while (node && node.next) {
+      node = node.next;
+      length += 1;
+    }
+    return length;
+  }
+
+  has(item) {
+    for (const value of this) {
+      if (isEqual(value, item)) return true;
+    }
+    return false;
   }
 
   append(item) {
@@ -49,9 +82,12 @@ export default class LinkedList {
   }
 
   insert(item, position) {
+    if (typeof position !== 'number' && position !== undefined && position !== null) {
+      throw TypeError('position type error');
+    }
     const newNode = new LinkedListNode(item);
-    let node = this.head;
     let index = 0;
+    let node = this.head;
 
     while (node && node.next && index !== position - 1) {
       node = node.next;
@@ -68,7 +104,56 @@ export default class LinkedList {
     node.next = newNode;
   }
 
-  delete(position) {
-    
+  removeFrom(position) {
+    if (position === undefined || position === null) {
+      this.removeFrom(this.getLength() > 0 ? this.getLength() - 1 : 0);
+      return;
+    }
+    let index = 0;
+    let node = this.head;
+    while (node && node.next && index !== position - 1) {
+      node = node.next;
+      index += 1;
+    }
+    if (position > this.getLength() - 1 || position < 0) {
+      throw new RangeError(`${position} is out of range of 0 - ${this.getLength() - 1}`);
+    }
+    if (position === this.getLength() - 1) {
+      this.tail = node;
+    }
+    node.next = node.next.next;
+  }
+
+  insertAfter(value, item, times = 1) {
+    const isRequiredValue = (nodeValue, count) => isEqual(nodeValue, value) && count === times;
+    const newNode = new LinkedListNode(item);
+    let node = this.head;
+    let count = 1;
+    while (node && node.next && !isRequiredValue(node.value, count)) {
+      if (node.value === value) count += 1;
+      node = node.next;
+    }
+    if (!node.next && !isEqual(node.value, value)) {
+      throw new RangeError(`can not find ${value}`);
+    }
+    newNode.next = node.next;
+    node.next = newNode;
+    if (!node.next.next) this.tail = newNode;
+  }
+
+  remove(item) {
+    let node = this.head;
+    let preNode = this.head;
+    while (node && node.next && !isEqual(item, node.value)) {
+      preNode = node;
+      node = node.next;
+    }
+    if (!isEqual(item, node.value)) throw new RangeError(`can not find ${item}`);
+    if (preNode === node) {
+      this.head = this.head.next;
+      return;
+    }
+    preNode.next = node.next;
+    if (!node.next) this.tail = preNode;
   }
 }
